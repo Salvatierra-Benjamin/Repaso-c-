0c26255d36b269f6dceecd01947639f7890092ee
Benjamin Salvatierra <bsalvatierratola@frba.utn.edu.ar>
2025-03-10 00:15:26 -0300

    no se que poner

--- Clases Yamila/Clase 3/Ej1.cpp
+++ Clases Yamila/Clase 3/Ej1.cpp
@@ -1,1 +1,19 @@
+/*
+
+Desarrollar una función que calcule el máximo común divisor de dos números enteros A, B
+con el siguiente algoritmo:
+1. Dividir A por B, y calcular el resto (0 < R < B)
+2. Si R = 0, el MCD es B, si no seguir en 3.
+3. Reemplazar A por B, B por R, y volver al paso 1.
+
+*/
+
 #include <iostream>
+using namespace std;
+
+int main()[
+
+
+
+
+]
